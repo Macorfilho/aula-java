56eb67c3d7377f6c27517cc244bf40948758c06e
Marcelo <macorfilho@gmail.com>
2025-02-20 10:46:57 -0300

    Aula Java - Iniciando com Spring Boot

--- src/main/java/com/fiap/ecb/aula_java/AulaJavaApplication.java
+++ src/main/java/com/fiap/ecb/aula_java/AulaJavaApplication.java
@@ -1,4 +1,4 @@
-package com.fiap._ecb.aula_java;
+package com.fiap.ecb.aula_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
